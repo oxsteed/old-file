959a53ca34f48479b8d6cbc18f738894cee8acaf
oxsteed <emilejeanphilippe1@gmail.com>
2026-03-21 14:39:09 -0400

    emile notifications.js

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const authenticate = require('../middleware/authenticate');
-const { getNotifications, markRead, markAllRead, deleteNotification } = require('../controllers/notificationController');
+const router      = require('express').Router();
+const { authenticate } = require('../middleware/auth');
+const notifCtrl   = require('../controllers/notificationController');
 
-router.get('/', authenticate, getNotifications);
-router.put('/:id/read', authenticate, markRead);
-router.put('/read-all', authenticate, markAllRead);
-router.delete('/:id', authenticate, deleteNotification);
+router.get('/',                  authenticate, notifCtrl.getNotifications);
+router.post('/mark-read',        authenticate, notifCtrl.markRead);
+router.get('/preferences',       authenticate, notifCtrl.getPreferences);
+router.put('/preferences',       authenticate, notifCtrl.updatePreferences);
+router.post('/push-token',       authenticate, notifCtrl.registerPushToken);
 
 module.exports = router;
