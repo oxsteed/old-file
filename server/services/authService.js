58a68dde065199ccf711893cd15d521f487b700d
oxsteed <emilejeanphilippe1@gmail.com>
2026-03-21 16:18:04 -0400

    Add authService.js with verifyToken for adminAuth middleware

---
+++ server/services/authService.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
+}
+
+module.exports = { verifyToken };
