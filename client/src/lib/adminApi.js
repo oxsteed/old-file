f5bed3ef6f1fc9f02e83e3fb437b57707ecc3cfb
oxsteed <emilejeanphilippe1@gmail.com>
2026-03-21 13:16:37 -0400

    adminApi.js

---
+++ client/src/lib/adminApi.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const adminApi = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
+  withCredentials: true
+});
+
+// Attach token automatically
+adminApi.interceptors.request.use(config => {
+  const token = localStorage.getItem('oxsteed_token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// Redirect to admin login on 401/403
+adminApi.interceptors.response.use(
+  res => res,
+  err => {
+    if ([401, 403].includes(err.response?.status)) {
+      window.location.href = '/admin/login';
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default adminApi;
