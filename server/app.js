ac7c618492913f834ba6043133c0eb3f51509e74
oxsteed <emilejeanphilippe1@gmail.com>
2026-03-21 14:42:56 -0400

    app.js

--- server/app.js
+++ server/app.js
@@ -1,2 +1,11 @@
 const adminRoutes = require('./routes/admin');
 app.use('/api/admin', adminRoutes);
+
+const notificationRoutes = require('./routes/notifications');
+const reviewRoutes       = require('./routes/reviews');
+const disputeRoutes      = require('./routes/disputes');
+
+app.use('/api/notifications', notificationRoutes);
+app.use('/api/reviews',       reviewRoutes);
+app.use('/api/disputes',      disputeRoutes);
+
