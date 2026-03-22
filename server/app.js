fc8603514d8607ac112af1c187f43a470c52bbfb
oxsteed <emilejeanphilippe1@gmail.com>
2026-03-22 14:51:54 -0400

    Phase 6: Register consent routes in app.js

--- server/app.js
+++ server/app.js
@@ -4,8 +4,10 @@ app.use('/api/admin', adminRoutes);
 const notificationRoutes = require('./routes/notifications');
 const reviewRoutes       = require('./routes/reviews');
 const disputeRoutes      = require('./routes/disputes');
+const consentRoutes     = require('./routes/consent');
 
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/reviews',       reviewRoutes);
 app.use('/api/disputes',      disputeRoutes);
+app.use('/api/consent',      consentRoutes);
 
