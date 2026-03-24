4f79b6ee1c0b4eb86dd9a207adb10d5fbc58ab75
oxsteed <emilejeanphilippe1@gmail.com>
2026-03-24 11:22:30 -0400

    Update privacy.jsFix privacy.js: remove destructuring on authenticate import

--- server/routes/privacy.js
+++ server/routes/privacy.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const { authenticate } = require('../middleware/authenticate');
-
+const authenticate = require('../middleware/authenticate');
 /**
  * CCPA/CPRA Privacy Rights Routes
  * Provides data export, account deletion, and data portability
