f1425bb1ca1c0fac077ec282918e433745b54b40
oxsteed <emilejeanphilippe1@gmail.com>
2026-04-03 23:55:14 -0400

    Create index.ts

---
+++ client/src/components/registration/index.ts
@@ -0,0 +1,7 @@
+// client/src/components/registration/index.ts
+export { default as StepBar } from './StepBar';
+export { default as AvatarUpload } from './AvatarUpload';
+export { default as OTPInput } from './OTPInput';
+export { default as PasswordField } from './PasswordField';
+export { default as DiditPanel } from './DiditPanel';
+export { default as AccountForm } from './AccountForm';
