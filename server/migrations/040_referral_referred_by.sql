0bc183c2f08217a651c5b23eab28cf7872dff2fb
oxsteed <emilejeanphilippe1@gmail.com>
2026-04-07 07:28:01 -0400

    Update 040_referral_referred_by.sql

--- server/migrations/040_referral_referred_by.sql
+++ server/migrations/040_referral_referred_by.sql
@@ -1,6 +1,6 @@
 -- Add referred_by to track who referred each user
 ALTER TABLE users
-  ADD COLUMN IF NOT EXISTS referred_by INTEGER REFERENCES users(id) ON DELETE SET NULL;
+  ADD COLUMN IF NOT EXISTS referred_by UUID REFERENCES users(id) ON DELETE SET NULL;
 
 CREATE INDEX IF NOT EXISTS idx_users_referred_by ON users(referred_by);
 
